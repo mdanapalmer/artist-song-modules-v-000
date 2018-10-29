c9b42966e55bef4fe6d60115339123ff9f6246b7
Dana Palmer <roiai4@hotmail.com>
2018-10-29 01:33:25 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,19 +1,32 @@
+require 'pry'
+
 class Artist
   attr_accessor :name
+  attr_reader :songs
+
   extend Memorable::ClassMethods
+  extend Findable::ClassMethods
   include Memorable::InstanceMethods
-  include Paramable
+  include Paramable::InstanceMethods
+
+  @@artists = []
+
+  def self.all
+    @@artists
+  end
 
   def initialize
     super
-      @songs << []
+    @songs = []
   end
 
-  @@all = []
-
-  def self.all
-    @@all
+  def add_song(song)
+    @songs << song
+    song.artist = self unless song.artist
   end
 
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
 
 end
