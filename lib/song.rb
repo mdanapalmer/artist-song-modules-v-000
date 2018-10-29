c5093bebc79e26775571563a6db77ec627aef321
Dana Palmer <roiai4@hotmail.com>
2018-10-29 01:33:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,24 @@
-class Song
-  extend Memorable::ClassMethods
+require 'pry'
 
-def initialize
-end
+class Song
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
+
+  attr_accessor :name
+  attr_reader :artist
+
+  @@songs = []
+
+  def self.all
+    @@songs
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self) unless artist.songs.include?(self)
+  end
 
 end
