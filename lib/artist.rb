d785bf0f703cff20b9473166d69b6ee4c4b6f5bd
Dana Palmer <roiai4@hotmail.com>
2018-10-29 01:30:22 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ class Artist
   attr_accessor :name
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
-  include Paramble::InstanceMethods
+  include Paramable::InstanceMethods
 
   def initialize
     super
